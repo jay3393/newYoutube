a92489084c432d699e4892eb5371ad89521eaa0e
Jason Liu <jhliu4@buffalo.edu>
2022-09-10 03:41:26 -0400

    finished project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Important imports are React, reactDom, App
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App'; 
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
